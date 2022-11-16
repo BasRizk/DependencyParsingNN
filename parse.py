dd8ed63e458e271e153d47ccd86582cfa1a2ac6d
basrizk <basem.rizk@outlook.com>
2022-11-16 03:15:24 -0800

    comments + cleaning + start parse

--- parse.py
+++ parse.py
@@ -1,16 +1,33 @@
-# TODO • parse.py, given a trained model file (and possibly vocabulary file) reads in CoNLL data and writes
-# CoNLL data where fields 7 and 8 contain dependency tree info. The syntax of the file should be python
-# parse.py -m [modelfile] -i [inputfile] -o [outputfile].
+import argparse
+from data_utils import BasicWordEncoder
+from data_utils import Sentence, Token
+from train import Model
 
+"""_summary_
+Given a trained model file (and possibly vocabulary file) reads in CoNLL data and writes
+CoNLL data where fields 7 and 8 contain dependency tree info.
 
+TODO High-level functions you may want to have in your parser class include initializing to the
+start state for a sentence, performing an action, getting the features of a current parse state, getting
+the ground truth action. The provided skeleton is one such way to develop your code, though more
+supporting functions are almost certainly a good idea.
+"""
 
-# High-level functions you may want to have in your parser class include initializing to the
-# start state for a sentence, performing an action, getting the features of a current parse state, getting
-# the ground truth action. The provided skeleton is one such way to develop your code, though more
-# supporting functions are almost certainly a good idea.
-
-# parse.py should take in a sentence and from the parser configuration
-# generate labels, which will determine each subsequent parser configuration (from which features can
-# be determined). Both files, then, should point to a third library file which contains code that, given
-# configuration at time t ct and label l, determines ct+1.
-
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parser file configuration.')
+    parser.add_argument('-m', type=str, help='model file including vocab (encoding)')
+    parser.add_argument('-i', type=str, help='input file')
+    parser.add_argument('-o', type=str, help='output file')
+    args = parser.parse_args()
+    
+    model = Model.load_model(args.m)
+    
+    # TODO Read data from input file args.i
+    # TODO Preprocess input data
+    # parse.py should take in a sentence and from the parser configuration
+    # generate labels, which will determine each subsequent parser configuration (from which features can
+    # be determined). Both files, then, should point to a third library file which contains code that, given
+    # configuration at time t ct and label l, determines ct+1.
+    # TODO use model properly    
+    # TODO output CoNLL formatted file with depend tree info aka. field 7 & 8
+    
--- prepare_data.py
+++ prepare_data.py
@@ -1,32 +1,15 @@
-# preparedata.py converts CoNLL data (train and dev) into features of the parser configuration paired
-# with parser decisions. This should be human-readable, i.e. a text file of words/labels. 
-# TODO The format should be described in the README
-# preparedata.py should take in a dependency tree and, based on the heuristics discussed in class,
-# determine parser actions, which will alter the parser configuration, from which the feature set can
-# be determined. Meanwhile 
-
-#################################################################################
-# Each line is in CoNLL data format, and has 10 tab-separated fields as follows:
-# 1. current sentence word number (1-based)
-# 2. the word
-# 3. lemma form of the word
-# 4. coarse-grained POS tag
-# 5. fine-grained POS tag (in practice, both of these are the same)
-# 6. morphological features (in practice, just the token ‘-’)
-# 7. token id of this word’s parent (head),
-# or 0 if this word’s parent is the root. 
-# If this data has not yet been provided 
-# (i.e. the sentence isn’t parsed) it is ’-’.
-# 8. dependency label for the relation of this word to its parent.
-# If this data has not yet been provided (i.e.
-# the sentence isn’t parsed) it is ’-’.
-# 9. other dependencies (in practice, just ‘-’)
-# 10. other info (in practice, just ‘-’)
-#################################################################################
-
-
-import pandas as pd
 from data_utils import FeatureGenerator, Sentence, Token
+
+"""_summary_
+preparedata.py converts CoNLL data (train and dev) into features of the parser configuration paired
+with parser decisions. This should be human-readable, i.e. a text file of words/labels. 
+TODO The format should be described in the README
+preparedata.py should take in a dependency tree and, based on the heuristics discussed in class,
+determine parser actions, which will alter the parser configuration, from which the feature set can
+be determined. Meanwhile 
+CoNLL File Explanation is provided below the code
+"""
+
 
 def read_parse_tree(filepath="train.orig.conll"):
     def split_conll_line(file):
@@ -76,3 +59,24 @@ if __name__ == "__main__":
         out_filepath = f'{filepath.split(".")[0]}.converted'
         train_dataset.to_csv(out_filepath, index=False)
         print(f"Written {out_filepath}")        
+        
+#################################################################################
+# CoNLL File Explanation
+#################################################################################
+# Each line is in CoNLL data format, and has 10 tab-separated fields as follows:
+# 1. current sentence word number (1-based)
+# 2. the word
+# 3. lemma form of the word
+# 4. coarse-grained POS tag
+# 5. fine-grained POS tag (in practice, both of these are the same)
+# 6. morphological features (in practice, just the token ‘-’)
+# 7. token id of this word’s parent (head),
+# or 0 if this word’s parent is the root. 
+# If this data has not yet been provided 
+# (i.e. the sentence isn’t parsed) it is ’-’.
+# 8. dependency label for the relation of this word to its parent.
+# If this data has not yet been provided (i.e.
+# the sentence isn’t parsed) it is ’-’.
+# 9. other dependencies (in practice, just ‘-’)
+# 10. other info (in practice, just ‘-’)
+#################################################################################
